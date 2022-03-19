3a5e84d29e1d66f8a56841b4b343ef818c32a9b1
YenaZh <yenazhandar@gmail.com>
2022-03-19 10:58:47 -0500

    Accepted!!!

---
+++ src/test/java/tests/YenaZhTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class YenaZhTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Privet Yena");
+        System.out.println("Welcome to IT world");
+    }
+}
