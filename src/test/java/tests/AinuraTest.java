f610bb9e5d1e72f94001a8f0fd338eab9ac27061
Azula-Dj <azulafire1377@gmail.com>
2022-03-19 10:59:14 -0500

    Committing and push

---
+++ src/test/java/tests/AinuraTest.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class AinuraTest {
+    public static void main(String[] args) {
+        System.out.println("Hey guys let's celebrate Nooruz on Sunday at campus");
+    }
+}
